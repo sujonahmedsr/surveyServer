777c94fa3b304e29a972cd4e4c6e8d520822cc8b
Shofiqul Islam <110051682+sujonahmedsr@users.noreply.github.com>
2024-06-06 15:14:53 +0600

    jwt token added localstorage

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const jwt = require('jsonwebtoken')
 require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
@@ -37,7 +38,19 @@ async function run() {
 
     const surveyCollections = client.db('surveySky').collection('survey')
     const usersCollections = client.db('surveySky').collection('users')
+    const commentsCollections = client.db('surveySky').collection('comments')
 
+
+    // for jwt token
+
+    app.post('/jwt', async (req, res) => {
+      const user = req.body
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '7d' });
+      res.send({ token })
+    })
+
+
+    // for survey operation 
     app.get('/survey', async (req, res) => {
       const cursor = surveyCollections.find()
       const result = await cursor.toArray()
@@ -51,7 +64,34 @@ async function run() {
       res.send(result)
     })
 
+    app.patch('/survey/:id', async(req,res) =>{
+      const id = req.params.id
+      const query = { _id : new ObjectId(id)}
+      const options = { upsert: true };
+      const findeone = await surveyCollections.findOne(query)
+      const updateVote = {
+        $set:{
+          votes : findeone.votes + 1
+        }
+      }
+      const result = await surveyCollections.updateOne(query, updateVote, options)
+      res.send(result)
+    })
 
+    // for comments 
+    app.get('/comments', async (req, res) => {
+      const cursor = commentsCollections.find()
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+
+    app.post('/comments', async(req, res)=>{
+      const userCoInfo = req.body
+      const result = await commentsCollections.insertOne(userCoInfo)
+      res.send(result)
+    })
+
+    // for users operation 
     app.post('/users', async(req, res)=>{
       const userInfo = req.body
       const query = {email : userInfo.email}
